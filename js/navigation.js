e13c2d26e2a626a39b629ee5e1f8232eafbd84ec
Elliissa01 <isa.herode@gmail.com>
2024-09-29 19:53:29 -0400

    [STYLE] Animation du bouton Quiz

--- js/navigation.js
+++ js/navigation.js
@@ -1,5 +1,6 @@
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
+const btnElement = document.querySelector(".nav-cta-button");
 let menuOpen = false;
 
 //Affiche ou cache le menu burger au clic.
@@ -11,4 +12,13 @@ hamburger.addEventListener("click", () => {
         navLinks.style.display = "none";
         menuOpen = false;
     }
+})
+
+//Animation du bouton Quiz.
+btnElement.addEventListener("mouseover", (event) => {
+    const x = (event.pageX - btnElement.offsetLeft);
+    const y = (event.pageY - btnElement.offsetTop);
+
+    btnElement.style.setProperty("--xPos", x + "px");
+    btnElement.style.setProperty("--yPos", y + "px");
 })
